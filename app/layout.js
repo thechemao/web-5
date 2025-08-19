2fe5dbaca4ca4e4db0afd8eb9d1fce57a09fd404
thechemao <xemagarrigos@gmail.com>
2025-08-19 03:54:13 +0200

    SpeedInsights

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Press_Start_2P } from 'next/font/google'
 import './globals.css'
 
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
         <UpperLink/>
         {children}
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   )
